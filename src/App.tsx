0d1999cc6abdfe786e9354994ee76e5a22c4e325
Rodion <rodion.chubarov@mail.ru>
2025-11-02 21:15:19 +0500

    first commit10

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// ...existing code...
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Admin } from "./pages/Admin";
@@ -19,3 +19,4 @@ function App() {
 }
 
 export default App;
+// ...existing code...
